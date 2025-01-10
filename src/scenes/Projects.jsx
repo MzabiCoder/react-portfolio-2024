3f460777a0e2fdeb852329640549128a429a1e1e
nabil <mzabi30@gmail.com>
2025-01-09 18:17:41 -0800

    change pic

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -15,7 +15,7 @@ const projectVariant = {
   visible: { opacity: 1, scale: 1 },
 };
 
-const Project = ({ image, github, project, title }) => {
+const Project = ({ image, github, project, title, login }) => {
   const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
   bg-amber-300 z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
   const projectTitle = title.split(" ").join("-").toLowerCase();
@@ -23,7 +23,9 @@ const Project = ({ image, github, project, title }) => {
   return (
     <motion.div variants={projectVariant} className="relative">
       <div className={overlayStyles}>
-       <div className="socials">{github}{project}</div> 
+        <div className="socials">{github}{project}</div>
+        {login && <div className="pt-12 font-semibold"><h1>login: nabil@nabil.com</h1>
+          <h1>passowrd: 12345</h1></div>}
       </div>
       <img src={image} alt={projectTitle} />
     </motion.div>
@@ -31,69 +33,69 @@ const Project = ({ image, github, project, title }) => {
 };
 
 const projectDetails1 = {
-  image:'https://images.pexels.com/photos/4691211/pexels-photo-4691211.jpeg?auto=compress&cs=tinysrgb&w=1600',
-  github:(
-        <a href="https://github.com/MzabiCoder/winerie" rel="noreferrer" target="_blank">
-        <i class="fab fa-github fa-3x"></i>
-      </a>
-        ),
-   project:(
-          <a href="https://mzabicoder.github.io/winerie/" rel="noreferrer" target="_blank">
-          <i class="fas fa-eye fa-3x"></i>
-        </a>
-      )     
+  image: 'https://images.pexels.com/photos/4691211/pexels-photo-4691211.jpeg?auto=compress&cs=tinysrgb&w=1600',
+  github: (
+    <a href="https://github.com/MzabiCoder/winerie" rel="noreferrer" target="_blank">
+      <i class="fab fa-github fa-3x"></i>
+    </a>
+  ),
+  project: (
+    <a href="https://mzabicoder.github.io/winerie/" rel="noreferrer" target="_blank">
+      <i class="fas fa-eye fa-3x"></i>
+    </a>
+  )
 }
 const projectDetails2 = {
-  image:'https://images.pexels.com/photos/2102934/pexels-photo-2102934.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-  github:(
-        <a href="https://github.com/MzabiCoder/ComptoirRestaurant" rel="noreferrer"  target="_blank">
-        <i class="fab fa-github fa-3x"></i>
-      </a>
-        ),
-   project:(
-          <a href="https://mzabicoder.github.io/ComptoirRestaurant/" rel="noreferrer" target="_blank">
-          <i class="fas fa-eye fa-3x"></i>
-        </a>
-      )     
+  image: 'https://images.pexels.com/photos/2102934/pexels-photo-2102934.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+  github: (
+    <a href="https://github.com/MzabiCoder/ComptoirRestaurant" rel="noreferrer" target="_blank">
+      <i class="fab fa-github fa-3x"></i>
+    </a>
+  ),
+  project: (
+    <a href="https://mzabicoder.github.io/ComptoirRestaurant/" rel="noreferrer" target="_blank">
+      <i class="fas fa-eye fa-3x"></i>
+    </a>
+  )
 }
 const projectDetails3 = {
-  image:'https://images.pexels.com/photos/16666938/pexels-photo-16666938/free-photo-of-elegant-flatware-on-table-in-restaurant.jpeg?auto=compress&cs=tinysrgb&w=1600',
-  github:(
-        <a href="https://github.com/MzabiCoder/reactProjec" rel="noreferrer" target="_blank">
-        <i class="fab fa-github fa-3x"></i>
-      </a>
-        ),
-   project:(
-          <a href="http://MzabiCoder.github.io/reactProjec" rel="noreferrer" target="_blank">
-          <i class="fas fa-eye fa-3x"></i>
-        </a>
-      )     
+  image: 'https://images.pexels.com/photos/16666938/pexels-photo-16666938/free-photo-of-elegant-flatware-on-table-in-restaurant.jpeg?auto=compress&cs=tinysrgb&w=1600',
+  github: (
+    <a href="https://github.com/MzabiCoder/reactProjec" rel="noreferrer" target="_blank">
+      <i class="fab fa-github fa-3x"></i>
+    </a>
+  ),
+  project: (
+    <a href="http://MzabiCoder.github.io/reactProjec" rel="noreferrer" target="_blank">
+      <i class="fas fa-eye fa-3x"></i>
+    </a>
+  )
 }
 const projectDetails4 = {
-  image:'https://images.pexels.com/photos/3434523/pexels-photo-3434523.jpeg?auto=compress&cs=tinysrgb&w=1600',
-  github:(
-        <a href="https://github.com/MzabiCoder/mikisteakhouse_business" rel="noreferrer" target="_blank">
-        <i class="fab fa-github fa-3x"></i>
-      </a>
-        ),
-   project:(
-          <a href="https://mzabicoder.github.io/mikisteakhouse_business/" rel="noreferrer" target="_blank">
-          <i class="fas fa-eye fa-3x"></i>
-        </a>
-      )     
+  image: 'https://images.pexels.com/photos/3434523/pexels-photo-3434523.jpeg?auto=compress&cs=tinysrgb&w=1600',
+  github: (
+    <a href="https://github.com/MzabiCoder/mikisteakhouse_business" rel="noreferrer" target="_blank">
+      <i class="fab fa-github fa-3x"></i>
+    </a>
+  ),
+  project: (
+    <a href="https://mzabicoder.github.io/mikisteakhouse_business/" rel="noreferrer" target="_blank">
+      <i class="fas fa-eye fa-3x"></i>
+    </a>
+  )
 }
 const projectDetails5 = {
-  image:'https://images.pexels.com/photos/5632379/pexels-photo-5632379.jpeg?auto=compress&cs=tinysrgb&w=800',
-  github:(
-        <a href="https://github.com/MzabiCoder/MERNFinal" rel="noreferrer" target="_blank">
-        <i class="fab fa-github fa-3x"></i>
-      </a>
-        ),
-   project:(
-          <a href="https://myshop-sl5l.onrender.com/" rel="noreferrer" target="_blank">
-          <i class="fas fa-eye fa-3x"></i>
-        </a>
-      )     
+  image: 'https://images.pexels.com/photos/5632379/pexels-photo-5632379.jpeg?auto=compress&cs=tinysrgb&w=800',
+  github: (
+    <a href="https://github.com/MzabiCoder/MERNFinal" rel="noreferrer" target="_blank">
+      <i class="fab fa-github fa-3x"></i>
+    </a>
+  ),
+  project: (
+    <a href="https://myshop-sl5l.onrender.com/" rel="noreferrer" target="_blank">
+      <i class="fas fa-eye fa-3x"></i>
+    </a>
+  )
 }
 
 const Projects = () => {
@@ -120,7 +122,7 @@ const Projects = () => {
           </div>
         </div>
         <p className="mt-10 mb-10">
-          
+
         </p>
       </motion.div>
 
@@ -144,9 +146,9 @@ const Projects = () => {
           <Project github={projectDetails2.github} project={projectDetails2.project} image={projectDetails2.image} title="Project 2" />
 
           {/* ROW 2 */}
-          <Project github={projectDetails3.github} project={projectDetails3.project} image={projectDetails3.image}title="Project 3" />
+          <Project github={projectDetails3.github} project={projectDetails3.project} image={projectDetails3.image} title="Project 3" />
           <Project github={projectDetails4.github} project={projectDetails4.project} image={projectDetails4.image} title="Project 4" />
-          <Project github={projectDetails5.github} project={projectDetails5.project} image={projectDetails5.image} title="Project 5" />
+          <Project login={true} github={projectDetails5.github} project={projectDetails5.project} image={projectDetails5.image} title="Project 5" />
 
           {/* ROW 3 */}
           {/* <Project title="Project 6" />
